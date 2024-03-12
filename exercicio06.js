a6ad048881ab056d956fad1880b44d8ae8266ddc
beehsouza-th <brenda.souza@estudante.ifms.edu.br>
2024-03-12 11:50:13 -0400

    Update exercicio06.js

--- exercicio06.js
+++ exercicio06.js
@@ -1,11 +1,15 @@
 function execuçãoComAtraso(tempo){
-    if(time.sleep(5)){
+    if(tempo < 5){
      throw new Error ("Ocorreu um erro");
     }
     return tempo
 }
  try{
-   const operacao = execuçãoComAtraso(5000);
+   const operacao = execuçãoComAtraso (5000);
+   setTimeout(() => {
+    console.log("Delayed for 1 second.");
+  }, "5000");
+  
  console.log("Operação executada com sucesso!");
 } catch(error){
     ("Ocorreu um erro durante a execução" , error.message);
